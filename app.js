061eaf37403fd298bc953433b2758e84bc718cae
mayconslv <mayconslv.contato@gmail.com>
2025-01-17 00:51:43 -0300

    function to randomly pick a friend

--- app.js
+++ app.js
@@ -10,10 +10,17 @@ function renderFriend(friendName) {
   const friendNameListItem = document.createElement("li");
   const friendNameListContainer = document.querySelector(".name-list");
   friendNameListContainer.appendChild(friendNameListItem);
-  friendsList.push(friendName);
-
   document.querySelector(".input-name").value = ''; // reset input value on submit
+  
+  friendsList.push(friendName);
 
   friendNameListItem.classList.add('friendNameListItem');
   friendNameListItem.textContent = friendName;
+}
+
+function sortearAmigo() {
+  const selectedFriend = document.querySelector(".result-list");
+  const selectedNumber = Math.floor(Math.random() * friendsList.length);
+  
+  selectedFriend.textContent = friendsList[selectedNumber];
 }
